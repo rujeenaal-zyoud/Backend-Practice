9b009a53f4042fc1551152617f16ef41cc1ab099
rujeenaal-zyoud <rujeenaalzoud@gmail.com>
2021-06-11 16:10:49 +0300

    2

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 require('dotenv').config();
- //const mongoose =require('mongoose');
+ const mongoose =require('mongoose');
 
  const server = express();
  server.use(cors());
@@ -12,16 +12,34 @@ require('dotenv').config();
 
  const PORT=process.env.PORT;
 
+
+mongoose.connect('mongodb://localhost:27017/recipes',
+{ useNewUrlParser: true, useUnifiedTopology: true }); 
+
+
+const recipeSchema = new mongoose.Schema({
+    label :String,
+    image:String,
+    ingredientLines:Array,
+
+})
+const recipeModel = mongoose.model('recipe',recipeSchema)
  
+ server.get('/test', testHandler);
+ function testHandler(req, res) {
+    res.send('test route')
+}
+
+server.get('/', (request, response) => {
+    let str = 'hello from back end';
+    response.status(202).send(str);
+})
 
 
- function testHandaller(req,res){
-     res.send('Hello from Backend');
+server.get('*', (req, res) => {
+    res.status(404).send('not found');
+})
 
- }
-
-
- server.listen(PORT,()=>{
-     console.log(`listening on PORT ${PORT}`);
- })
- server.get('/',testHandaler);
+server.listen(PORT,()=>{
+    console.log(`listening on PORT ${PORT}`);
+})
