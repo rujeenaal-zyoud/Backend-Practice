0bffe418501edc00c7fb84806a4e3ab1fa73577c
rujeenaal-zyoud <rujeenaalzoud@gmail.com>
2021-06-11 23:25:08 +0300

    render-Card

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 require('dotenv').config();
- const mongoose =require('mongoose');
+ //const mongoose =require('mongoose');
 
  const server = express();
  server.use(cors());
@@ -13,19 +13,45 @@ require('dotenv').config();
  const PORT=process.env.PORT;
 
 
-mongoose.connect('mongodb://localhost:27017/recipes',
-{ useNewUrlParser: true, useUnifiedTopology: true }); 
+// mongoose.connect('mongodb://localhost:27017/recipes',
+// { useNewUrlParser: true, useUnifiedTopology: true }); 
 
 
-const recipeSchema = new mongoose.Schema({
-    label :String,
-    image:String,
-    ingredientLines:Array,
+// const recipeSchema = new mongoose.Schema({
+//     label :String,
+//     image:String,
+//     ingredientLines:Array,
 
-})
-const recipeModel = mongoose.model('recipe',recipeSchema)
+// })
+// const recipeModel = mongoose.model('recipe',recipeSchema)
  
  server.get('/test', testHandler);
+ server.get('/recipes',recipesHandler);
+//  server.post('/addToFav',favoriteHandler);
+//  server.get('/getFav',getFavoriteHandler);
+//  server.delete('/deleteRecipe/:id',deleteHandler);
+//  server.put('/updateRecipe/:id',updateHandler);
+
+//here request data from api to send the res to front end
+function recipesHandler(req,res){
+    const ingredient = req.query.ingredient;
+    console.log(ingredient)
+console.log(req.query);
+const url = `https://api.edamam.com/search?q=${ingredient}&app_id=${process.env.FOOD_APP_ID}&app_key=${process.env.FOOD_APP_KEY}`;
+
+axios
+.get(url)
+.then(result =>{
+    const recipesArray = result.data.hits.map(item=>{
+        return new Recipe(item);
+        console.log(recipesArray);
+    })
+    res.send(recipesArray);
+
+})
+}
+
+
  function testHandler(req, res) {
     res.send('test route')
 }
@@ -40,6 +66,16 @@ server.get('*', (req, res) => {
     res.status(404).send('not found');
 })
 
+// we put recipe after data becuse in JSON API we have object of array 
+// object hits  and recipe that have data
+class Recipe{
+    constructor(data){
+        this.label = data.recipe.label;
+        this.image= data.recipe.image;
+        this.ingredientLines = data.recipe.ingredientLines;
+
+    }
+}
 server.listen(PORT,()=>{
     console.log(`listening on PORT ${PORT}`);
 })
